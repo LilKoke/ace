20243f086d34c305bb431d221fba312dbf45b93e
lilkoke <k-hirano@wb3.so-net.ne.jp>
2023-02-07 12:17:43 +0900

    refac: use linalg and fix funcs

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,48 +1,69 @@
 #include <bits/stdc++.h>
 #include <sys/stat.h>
+#include "src/lin_alg.hpp"
 
 using namespace std;
 
-void calculate_domega(double omega[], double M[], double Iner[], double domega[]){
+LinAlg la;
+
+vector<double> calculate_domega(vector<double> omega, vector<double> M, vector<double> Iner){
+    vector<double> domega(4);
     domega[0] = 0;
-    domega[1] = ((Iner[2] - Iner[1]) * omega[1] * omega[2] - M[0]) / Iner[0]; 
-    domega[2] = ((Iner[0] - Iner[2]) * omega[0] * omega[2] - M[0]) / Iner[1];
-    domega[3] = ((Iner[1] - Iner[0]) * omega[0] * omega[1] - M[0]) / Iner[2]; 
+    domega[1] = (M[0] - (Iner[2] - Iner[1]) * omega[2] * omega[3]) / Iner[0]; 
+    domega[2] = (M[1] - (Iner[0] - Iner[2]) * omega[1] * omega[3]) / Iner[1];
+    domega[3] = (M[2] - (Iner[1] - Iner[0]) * omega[1] * omega[2]) / Iner[2];
+    return domega; 
 }
 
-void calculate_dq(double q[], double omega[], double dq[]){
-    dq[0] = (q[0] * omega[0] - q[1] * omega[1] - q[2] * omega[2] - q[3] * omega[3]) / 2;
-    dq[1] = (q[0] * omega[1] + q[1] * omega[0] + q[2] * omega[3] - q[3] * omega[2]) / 2;
-    dq[2] = (q[0] * omega[2] - q[1] * omega[3] + q[2] * omega[0] + q[3] * omega[1]) / 2;
-    dq[3] = (q[0] * omega[3] + q[1] * omega[2] - q[2] * omega[1] + q[3] * omega[0]) / 2;
+// vector<double> calculate_dq(vector<double> q, vector<double> omega){
+//     vector<double> dq(4);
+//     LinAlg la;
+//     vector<vector<double> > Omega = {
+//         {0, -omega[1], -omega[2], -omega[3]},
+//         {omega[1], 0, omega[3], -omega[2]},
+//         {omega[2], -omega[3], 0, omega[1]},
+//         {omega[3], omega[2], -omega[1], 0}
+//     };
+//     dq = la.squeeze(la.dot(Omega, la.T(q)));
+//     return dq;
+// }
+
+vector<double> calculate_dq(vector<double> q, vector<double> omega){
+    vector<double> dq(4);
+    dq = la.mult(0.5, la.qdot(q, omega));
+    return dq;
 }
 
-void update_q(double q[], double dq[], double dt){
-    double q_sum = 0;
-    for(int i = 0; i < 4; i++){
-        q[i] += dq[i] * dt;
-    }
-    for(int i = 0; i < 4; i++) q_sum += q[i];
-    for(int i = 0; i < 4; i++) q[i] /= q_sum;
-}
+// vector<double> calculate_dq(vector<double> q, vector<double> omega){
+//     vector<double> dq(4);
+//     vector<vector<double> > qmat = {
+//         {0, -q[1], -q[2], -q[3]},
+//         {0, q[0], -q[3], q[2]},
+//         {0, q[3], q[0], -q[1]},
+//         {0, -q[2], q[1], q[0]}
+//     };
+//     dq = la.squeeze(la.mult(0.5, la.dot(qmat, la.T(omega))));
+//     return dq;
+// }
+
 
 #define PI 3.1415
 
 int main(){
-    double q[4] = {1, 0, 0, 0};
-    double dq[4] = {0, 0, 0, 0};
+    vector<double> q = {1, 0, 0, 0};
+    vector<double> dq;
     double omega_s = 17 * 2 * PI / 60;
-    double omega[4] = {0, 0.1, omega_s + 0.1, 0};
-    double M[3] = {0, 0, 0};
-    double Iner[3] = {1.9, 1.6, 2.0};
-    double domega[3];
+    vector<double> omega = {0, 0.1, omega_s + 0.1, 0};
+    vector<double> M = {0, 0, 0};
+    vector<double> Iner = {1.9, 1.6, 2.0};
+    vector<double> domega;
     double t0 = 0;
-    double tn = 1;
-    double n = 100;
-    double h = (tn - t0) / n;
-    double k1_omega[4], k2_omega[4], k3_omega[4], k4_omega[4];
-    double k1_q[4], k2_q[4], k3_q[4], k4_q[4];
-    double omega_runge[4], q_runge[4];
+    double tn = 30;
+    double n = tn * 1000;
+    double h = (tn - t0)/n;
+    vector<double> k1_omega(4), k2_omega(4), k3_omega(4), k4_omega(4);
+    vector<double> k1_q(4), k2_q(4), k3_q(4), k4_q(4);
+    vector<double> omega_runge(4), q_runge(4);
     double t = 0;
     ofstream writing_file1, writing_file2;
     string folder_name = "out/assignment1";
@@ -58,40 +79,35 @@ int main(){
             omega_runge[i] = omega[i];
             q_runge[i] = q[i];
         }
-        calculate_domega(omega_runge, M, Iner, domega);
-        calculate_dq(q_runge, omega_runge, dq);
-        for(int i = 0; i < 4; i++){
-            k1_omega[i] = h * domega[i];
-            k1_q[i] = h * dq[i];
-            omega_runge[i] += k1_omega[i] / 2;
-            q_runge[i] += k1_q[i] / 2;
-        }
-        calculate_domega(omega_runge, M, Iner, domega);
-        calculate_dq(q_runge, omega_runge, dq);
-        for(int i = 0; i < 4; i++){
-            k2_omega[i] = h * domega[i];
-            k2_q[i] = h * dq[i];
-            omega_runge[i] += k2_omega[i] / 2;
-            q_runge[i] += k2_q[i] / 2;
-        }
-        calculate_domega(omega_runge, M, Iner, domega);
-        calculate_dq(q_runge, omega_runge, dq);
-        for(int i = 0; i < 4; i++){
-            k3_omega[i] = h * domega[i];
-            k3_q[i] = h * dq[i];
-            omega_runge[i] += k3_omega[i];
-            q_runge[i] += k3_q[i];
-        }
-        calculate_domega(omega_runge, M, Iner, domega);
-        calculate_dq(q_runge, omega_runge, dq);
-        for(int i = 0; i < 4; i++){
-            k4_omega[i] = h * domega[i];
-            k4_q[i] = h * dq[i];
-        }
+        domega = calculate_domega(omega_runge, M, Iner);
+        dq = calculate_dq(q_runge, omega_runge);
+        k1_omega = la.mult(h, domega);
+        k1_q = la.mult(h, dq);
+        omega_runge = la.add(omega_runge, la.mult(0.5, k1_omega));
+        q_runge = la.add(q_runge, la.mult(0.5, k1_q));
+
+        domega = calculate_domega(omega_runge, M, Iner);
+        dq = calculate_dq(q_runge, omega_runge);
+        k2_omega = la.mult(h, domega);
+        k2_q = la.mult(h, dq);
+        omega_runge = la.add(omega_runge, la.mult(0.5, k2_omega));
+        q_runge = la.add(q_runge, la.mult(0.5, k2_q));
+
+        domega = calculate_domega(omega_runge, M, Iner);
+        dq = calculate_dq(q_runge, omega_runge);
+        k3_omega = la.mult(h, domega);
+        k3_q = la.mult(h, dq);
+        omega_runge = la.add(omega_runge, k3_omega);
+        q_runge = la.add(q_runge, la.mult(1, k3_q));
+
+        domega = calculate_domega(omega_runge, M, Iner);
+        dq = calculate_dq(q_runge, omega_runge);
+        k4_omega = la.mult(h, domega);
+        k4_q = la.mult(h, dq);
 
         for(int i = 0; i < 4; i++){
-            omega[i] += k1_omega[i] + 2 * k2_omega[i] + 2 * k3_omega[i] + k4_omega[i];
-            q[i] += k1_q[i] + 2 * k2_q[i] + 2 * k3_q[i] + k4_q[i];
+            omega[i] += (k1_omega[i] + 2 * k2_omega[i] + 2 * k3_omega[i] + k4_omega[i])/6;
+            q[i] += (k1_q[i] + (2 * k2_q[i]) + (2 * k3_q[i]) + k4_q[i])/6;
         }
         t += h;
         writing_file1 << t << " " << omega[1] << " " << omega[2] << " " << omega[3] << endl;        
